80e4d6ba97da405f6ea125c188b6bf20599d9dc1
max <max.yuri.13@hotmail.com>
2023-11-16 16:54:34 -0300

    update 1

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import { Home } from './pages/Home';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  </React.StrictMode>
+)
