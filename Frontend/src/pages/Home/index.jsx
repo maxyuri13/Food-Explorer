fc8141a0d35417d3452d25750f76274255d7fe62
max <max.yuri.13@hotmail.com>
2023-11-24 18:02:48 -0300

    Banner 1.0

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import { Container } from "./styles";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer"
+import { Banner } from "../../components/Banner";
 
 export function Home() {
   
   return (
     <Container>
       <Header />
+      <Banner />
       <Footer />
     </Container>
   )
